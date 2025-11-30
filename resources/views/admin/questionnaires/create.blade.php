2a1b576d43b4f4982496ca670c93b0412d827b78
Annisa Rizky Maulida <annisarizkymaulida14@gmail.com>
2025-11-30 21:22:24 +0700

    tambah kuisioner

--- resources/views/admin/questionnaires/create.blade.php
+++ resources/views/admin/questionnaires/create.blade.php
@@ -54,25 +54,6 @@
                             <x-input-error :messages="$errors->get('description')" class="mt-2" />
                         </div>
 
-                        <!-- Sub-sections and Questions -->
-                        <div class="mb-6">
-                            <div class="flex items-center justify-between mb-4">
-                                <x-input-label :value="__('Sub-bab dan Pertanyaan')" />
-                                <button type="button" id="add-subsection" class="px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary-dark transition-colors text-sm">
-                                    <svg class="w-4 h-4 inline mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-                                    </svg>
-                                    Tambah Sub-bab
-                                </button>
-                            </div>
-
-                            <div id="subsections-container" class="space-y-6">
-                                <!-- Sub-sections will be added here dynamically -->
-                            </div>
-
-                            <x-input-error :messages="$errors->get('subsections')" class="mt-2" />
-                        </div>
-
                         <!-- Estimated Duration -->
                         <div class="mb-6">
                             <x-input-label for="estimated_duration" :value="__('Estimasi Durasi (menit)')" />
